9ac0d433dff146085ab7af64cab83d0b36fdf831
iomatter <30539354+iomatter@users.noreply.github.com>
2017-09-15 16:06:40 -0700

    Fixed broken code
    All im gonna say is ...
    ...
    ...
    NOTHING

--- onBoot/masterboot.update.py
+++ onBoot/masterboot.update.py
@@ -1,7 +1,19 @@
-import datetime
+from datetime import datetime
+from tkinter import messagebox as m
+from tkinter import Tk
+
 print("functional")
-date = datetime.time.now()
-version = "0.0.0"
-boot = open("record\boot.ini", "a")
-boot.write("[version[" + version + "]][date["+ date +"]]")
-boot.write("\n")
+version = 0.0
+strVersion = "0.0"
+def toBoot():
+    date = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+    boot = open("record\\boot.ini", "a")
+    boot.write("\n")
+    boot.write("[version[" + strVersion + "]][date["+ date +"]]")
+    boot.write("\n")
+    boot.close()
+def  if_prerealese():
+    if version < 3:
+        root=Tk()
+        m.showinfo("note", "Nytron Music is in a prerealese version. ("+ strVersion +") \n please dont mind certant features not being included. \n Thanks! (: ")
+        root.destroy()
